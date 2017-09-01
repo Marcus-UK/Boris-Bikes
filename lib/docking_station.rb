6a721273de1e585bd7bf84214cde71bf8b5b0c61
Marcus Rands <mrands@hotmail.co.uk>
2017-09-01 16:13:23 +0100

    modified dock method to return error if capacity reached

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,18 +4,19 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-    @bikes = 0
+    @bikes = []
   end
 
   def release_bike
-    raise "This is a fail" if @bikes < 1
-    raise "This is a fail" if !working
+    raise "This is a fail" unless @bike
+    #raise "This is a fail" if !working?
     @bike
     end
 
       #raise "this is a fail" if @bikes == 0
 
   def dock(bike)
-    @bike = bike
+    fail 'Dock full' if @bikes.count >=20
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,23 +6,26 @@ describe DockingStation do
   describe '#dock' do
    it 'docks bike' do
      bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    expect(subject.dock(bike)).to eq([bike])
   end
-
+end
   describe '#release_bike' do
   it 'release working bikes' do
     bike = Bike.new
     expect(bike).to be_working
   end
+end
 
-  describe '#dock' do
-   it 'docks bike' do
-     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
-  end
 
    it 'Raises an error when a bike is not present' do
      expect {subject.release_bike }.to raise_error 'This is a fail'
+   end
+
+   it "Tells you that Dock is full" do
+
+    20.times  {subject.dock Bike.new}
+
+     expect {subject.dock Bike.new}.to raise_error 'Dock full'
    end
 
 
