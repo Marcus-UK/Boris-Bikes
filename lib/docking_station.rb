e55807071505532c1b686c3025e3c5801ab3c5c8
Marcus Rands <mrands@hotmail.co.uk>
2017-09-01 14:55:46 +0100

    Updated tests to test raising an exception on release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,19 @@ require_relative 'bike.rb'
 
 class DockingStation
   attr_reader :bike
+
+  def initialize
+    @bikes = 0
+  end
+
   def release_bike
-    Bike.new
-  end
-  def bike
+    raise "This is a fail" if @bikes < 1
+    raise "This is a fail" if !working
     @bike
-  end
+    end
+
+      #raise "this is a fail" if @bikes == 0
+
   def dock(bike)
     @bike = bike
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,27 @@ require './lib/docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'release working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  describe '#dock' do
    it 'docks bike' do
      bike = Bike.new
     expect(subject.dock(bike)).to eq(bike)
   end
-  
+
+  describe '#release_bike' do
+  it 'release working bikes' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+
+  describe '#dock' do
+   it 'docks bike' do
+     bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+   it 'Raises an error when a bike is not present' do
+     expect {subject.release_bike }.to raise_error 'This is a fail'
+   end
+
+
 end
